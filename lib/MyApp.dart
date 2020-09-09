cbafe0a289b883a64bd4e1be007884f007bd49b1
ajay <ajaidonglee@gmail.com>
2020-09-09 12:04:15 +0530

    Done

--- lib/MyApp.dart
+++ lib/MyApp.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
-import 'MyHomePage.dart';
-
+import 'package:meals_app/Screen/CategoryScreen.dart';
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Meals App',
-      home: MyHomePage(),
+      theme: ThemeData(
+          primarySwatch: Colors.pink,
+          accentColor: Colors.amberAccent,
+          canvasColor: Color.fromRGBO(255, 254, 229, 1),
+          fontFamily: 'Raleway',
+          textTheme: ThemeData.light().textTheme.copyWith(
+                bodyText1: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                bodyText2: TextStyle(
+                  color: Color.fromRGBO(20, 51, 51, 1),
+                ),
+                headline6: TextStyle(
+                  fontSize: 24.0,
+                  fontFamily: 'RobotoCondensed',
+                ),
+              )),
+      home: CategoryScreen(),
     );
   }
 }
--- lib/Screen/CategoryScreen.dart
+++ lib/Screen/CategoryScreen.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
-
+import 'package:meals_app/models/dummy_data.dart';
+import 'package:meals_app/widget/Category_item.dart';
 class CategoryScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200,mainAxisSpacing: 20,crossAxisSpacing: 20,childAspectRatio: 3/2),
-      children: [
-
-      ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('meals'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(15),
+          gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 200,mainAxisSpacing: 20,crossAxisSpacing: 20,childAspectRatio: 3/2),
+          children: DUMMY_CATEGORIES.map((item) {
+            return CategoryItem(title: item.title ,color: item.color,);
+          }).toList()
+      ),
     );
   }
 }
